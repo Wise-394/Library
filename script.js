cba7c8dded0d22e54de0b4be9909928da03549a4
Wise-394 <114738222+Wise-394@users.noreply.github.com>
2026-03-19 20:08:11 +0800

    Added error message for forms

--- script.js
+++ script.js
@@ -91,6 +91,11 @@ class View {
 
     static addBook(e) {
         e.preventDefault();
+        if(!this.#validateForm()){
+            const errorMsg = document.querySelector("form > span")
+            errorMsg.textContent = "please fill up all the form with valid data"
+            return
+        }
         const formData = new FormData(this.form);
         Library.addBookToLibrary(
             formData.get("title"),
@@ -110,20 +115,63 @@ class View {
         View.displayBook();
     }
 
-    static #validate(){
+    static #initValidate(){
         const title = document.querySelector("#title")
-        title.addEventListener("input", (event) => {
-            if (title.validity.tooShort){
-                title.setCustomValidity("Title too short")
-                title.reportValidity()
-            } else if (title.validity.tooLong){
-                title.setCustomValidity("Title too long")
-                title.reportValidity()
-            }
-             else {
-                title.setCustomValidity("")
-            } 
-        })
+        title.addEventListener("input", () => this.#validateTitle())
+
+        const author = document.querySelector("#author")
+        author.addEventListener("input", () => this.#validateAuthor())
+
+        const pages = document.querySelector("#pages")
+        pages.addEventListener("input", () => this.#validatePages())
+    }
+
+    static #validateForm() {
+        const title = document.querySelector("#title")
+        const author = document.querySelector("#author")
+        const pages = document.querySelector("#pages")
+
+       return title.validity.valid && author.validity.valid && pages.validity.valid 
+    }
+
+    static #validateTitle() {
+        const title = document.querySelector("#title")
+        if (title.validity.tooShort) {
+            title.setCustomValidity("Title too short")
+            title.reportValidity()
+            title.classList.add("invalid")
+            return false
+        } else {
+            title.setCustomValidity("")
+            title.classList.remove("invalid")
+            return true
+        }
+    }
+    static #validateAuthor() {
+        const author = document.querySelector("#author")
+        if (author.validity.tooShort) {
+            author.setCustomValidity("Author too short")
+            author.reportValidity()
+            author.classList.add("invalid")
+            return false
+        } else {
+            author.setCustomValidity("")
+            author.classList.remove("invalid")
+            return true
+        }
+    }
+    static #validatePages() {
+        const pages = document.querySelector("#pages")
+        if (pages.validity.rangeUnderflow) {
+            pages.setCustomValidity("Must be larger than 0")
+            pages.reportValidity()
+            pages.classList.add("invalid")
+            return false
+        } else {
+            pages.setCustomValidity("")
+            pages.classList.remove("invalid")
+            return true
+        }
     }
 
     static init() {
@@ -131,7 +179,7 @@ class View {
         this.form.addEventListener("submit", (e) => this.addBook(e));
         this.deleteButton.addEventListener("click", () => this.closeModal());
 
-        this.#validate()
+        this.#initValidate()
     }
 
 }
